fd51b92f8f3921cc46d77a4771e0a511a807d55f
fxdmin <go.jminn@gmail.com>
2021-10-29 08:45:14 +0900

    vue component

--- src/router/index.js
+++ src/router/index.js
@@ -88,6 +88,19 @@ const routes = [
   {
     path: "/menu02/exam02directives",
     component : () => import(/* webpackChunkName: "menu02" */ '../views/menu02/Exam02Directives'),
+  },
+  {
+    path: "/menu02/exam03attrbinding",
+    component : () => import(/* webpackChunkName: "menu02" */ '../views/menu02/Exam03AttrBinding'),
+  },
+  {
+    path: "/menu02/exam04formbinding",
+    component : () => import(/* webpackChunkName: "menu02" */ '../views/menu02/Exam04FormBinding'),
+  }
+  ,
+  {
+    path: "/menu02/exam05repeatbinding",
+    component : () => import(/* webpackChunkName: "menu02" */ '../views/menu02/Exam05ForRepeatBinding'),
   }
 ]
 const router = new VueRouter({
--- src/views/menu02/Exam02Directives.vue
+++ src/views/menu02/Exam02Directives.vue
@@ -6,10 +6,25 @@
     </div>
     <div class="card-body">
       <div>
-        <img src="@/assets/photos/photo15.jpg" height="200px"/>
+        <img src="@/assets/photos/photo15.jpg" height="200px" class="mr-2"/>
         <!--### v-bind:src 는 :src와 동일하다 ###-->
         <img v-bind:src="require(`@/assets/photos/${photoFileName}`)" height="200px"/>
       </div>
+      <hr/>
+      
+      <!--요소 숨김 여부-->
+      <!--sale이라는 데이터 없으면 dom에 부착을 안시킴-->
+      <span v-if="sale" class="mr-2">
+        <img v-bind:src="require(`@/assets/photos/${photoFileName}`)" height="200px"/>
+      </span>
+      <!--sale이라는 데이터가 없어도 dom에 부착을 하고 visibility false(존재는 하는데 안보임 )-->
+      <span v-show="sale">
+        <img v-bind:src="require(`@/assets/photos/${photoFileName}`)" height="200px"/>
+      </span>
+      <hr/>
+
+      <p>정보 : {{info}}</p>
+      <p>정보 : <span v-html="info"></span></p>
       <hr/>
       <button class="btn btn-outline-dark" @click="changeData">변경</button>
     </div>
@@ -27,7 +42,9 @@ export default {
   //컴포넌트 데이터 정의
   data : function() {
     return {
-      photoFileName : "photo2.jpg"
+      photoFileName : "photo2.jpg",
+      sale : true,
+      info : `<div style="background-color : #eeeeee">스타일 적용 확인</div>`
     };
   },
   //컴포넌트 메서드 정의
@@ -38,6 +55,7 @@ export default {
       }else{
         this.photoFileName = "photo2.jpg";
       }
+      this.sale = !this.sale;
     }
   }
 }
